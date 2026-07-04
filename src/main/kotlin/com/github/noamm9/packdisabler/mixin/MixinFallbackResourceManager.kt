a33161e1df09b0a7e485038f047d0ee8388055c0
Noamm9 <belkunsama@gmail.com>
2026-07-05 02:41:38 +0300

    refactor: kotlin mixins

--- src/main/kotlin/com/github/noamm9/packdisabler/mixin/MixinFallbackResourceManager.kt
+++ src/main/kotlin/com/github/noamm9/packdisabler/mixin/MixinFallbackResourceManager.kt
@@ -1,5 +1,6 @@
 package com.github.noamm9.packdisabler.mixin
 
+import com.github.noamm9.packdisabler.FilteredPackResources
 import com.github.noamm9.packdisabler.ResourceOverrides
 import com.github.noamm9.packdisabler.config.Config
 import net.minecraft.resources.Identifier
@@ -8,36 +9,33 @@ import net.minecraft.server.packs.resources.FallbackResourceManager
 import org.spongepowered.asm.mixin.Final
 import org.spongepowered.asm.mixin.Mixin
 import org.spongepowered.asm.mixin.Shadow
-import org.spongepowered.asm.mixin.Unique
 import org.spongepowered.asm.mixin.injection.At
-import org.spongepowered.asm.mixin.injection.Inject
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo
+import org.spongepowered.asm.mixin.injection.ModifyArgs
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args
 import java.util.function.*
 
 @Mixin(FallbackResourceManager::class)
 class MixinFallbackResourceManager {
     @Shadow @Final private val namespace: String? = null
 
-    @Inject(method = ["push(Lnet/minecraft/server/packs/PackResources;)V"], at = [At("HEAD")], cancellable = true)
-    private fun skipHypixelPack(pack: PackResources, ci: CallbackInfo) {
-        skipHypixelPack(namespace !!, pack.packId(), ci)
-    }
-
-    @Inject(method = ["push(Lnet/minecraft/server/packs/PackResources;Ljava/util/function/Predicate;)V"], at = [At("HEAD")], cancellable = true)
-    private fun skipFilteredHypixelPack(pack: PackResources, filter: Predicate<Identifier?>?, ci: CallbackInfo) {
-        skipHypixelPack(namespace !!, pack.packId(), ci)
-    }
-
-    @Inject(method = ["pushFilterOnly"], at = [At("HEAD")], cancellable = true)
-    private fun skipHypixelPackFilter(packId: String, filter: Predicate<Identifier?>?, ci: CallbackInfo) {
-        skipHypixelPack(namespace !!, packId, ci)
-    }
-
-    @Unique
-    private fun skipHypixelPack(namespace: String, packId: String, ci: CallbackInfo) {
+    @ModifyArgs(
+        method = [
+            "pushFilterOnly",
+            "push(Lnet/minecraft/server/packs/PackResources;)V",
+            "push(Lnet/minecraft/server/packs/PackResources;Ljava/util/function/Predicate;)V"
+        ],
+        at = At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/server/packs/resources/FallbackResourceManager;pushInternal(Ljava/lang/String;Lnet/minecraft/server/packs/PackResources;Ljava/util/function/Predicate;)V"
+        )
+    )
+    private fun filterHypixelPack(args: Args) {
         if (! Config.disableGlobalPackOverrides) return
         if (namespace != Identifier.DEFAULT_NAMESPACE) return
-        if (! ResourceOverrides.fromHypixelPack(packId)) return
-        ci.cancel()
+        if (! ResourceOverrides.fromHypixelPack(args.get(0))) return
+
+        val pack = args.get<PackResources?>(1)
+        if (pack != null) args.set(1, FilteredPackResources(pack))
+        else args.set(2, Predicate<Identifier> { false })
     }
 }
--- src/main/kotlin/com/github/noamm9/packdisabler/mixin/MixinMinecraft.kt
+++ src/main/kotlin/com/github/noamm9/packdisabler/mixin/MixinMinecraft.kt
@@ -10,16 +10,16 @@ import org.spongepowered.asm.mixin.injection.Inject
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo
 
 //? if <26.2 {
-import net.minecraft.client.Minecraft
+/*import net.minecraft.client.Minecraft
 
-//?}
+*///?}
 //? if >=26.2 {
 //?}
 //? if >=26.2 {
-/*@Mixin(Gui::class) *///?} else {
-@Mixin(Minecraft::class)
+@Mixin(Gui::class) //?} else {
+/*@Mixin(Minecraft::class)
 
-//?}
+*///?}
 class MixinMinecraft {
     @Inject(method = ["setOverlay"], at = [At("HEAD")], cancellable = true)
     private fun onSetOverlay(overlay: Overlay?, ci: CallbackInfo) {
