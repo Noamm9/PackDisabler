22c7e467d510a0913f3bd565a1729fcf7f622ce3
Noamm9 <belkunsama@gmail.com>
2026-07-09 15:26:40 +0300

    fix build

--- src/main/kotlin/com/github/noamm9/packdisabler/config/Config.kt
+++ src/main/kotlin/com/github/noamm9/packdisabler/config/Config.kt
@@ -1,6 +1,5 @@
 package com.github.noamm9.packdisabler.config
 
-import com.github.noamm9.packdisabler.PackDisabler
 import dev.isxander.yacl3.api.*
 import dev.isxander.yacl3.api.controller.BooleanControllerBuilder
 import dev.isxander.yacl3.config.v2.api.ConfigClassHandler
@@ -21,21 +20,11 @@ class Config {
         fun createScreen(parent: Screen?) = YetAnotherConfigLib.create(handler) { defaults, config, builder ->
             builder.title(Component.literal("Pack Disabler")).category(ConfigCategory.createBuilder().apply {
                 name(Component.literal("General"))
-
                 option(Option.createBuilder<Boolean>().apply {
                     name(Component.literal("Block Pack Download"))
                     description(OptionDescription.of(Component.literal("Blocks the resource pack download packet sent by Hypixel when joining Skyblock.")))
                     binding(defaults.blockPackDownload, { config.blockPackDownload }, { config.blockPackDownload = it })
                     controller(BooleanControllerBuilder::create)
-                }.build())
-
-                option(ButtonOption.createBuilder().apply {
-                    name(Component.literal("Clear Cache"))
-                    description(OptionDescription.of(Component.literal("Clears the cache file of the item api.")))
-                    action { _, _ ->
-                        PackDisabler.cacheDir.toFile().deleteRecursively()
-                        PackDisabler.logger.info("Cleared cache file")
-                    }
                 }.build())
             }.build())
         }.generateScreen(parent)
