2de75592b51c3530a734b10a9cc408d88656375a
Noamm9 <belkunsama@gmail.com>
2026-08-20 22:22:36 +0300

    trim end of file

--- src/main/kotlin/com/github/noamm9/packdisabler/mixin/MixinFallbackResourceManager.kt
+++ src/main/kotlin/com/github/noamm9/packdisabler/mixin/MixinFallbackResourceManager.kt
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.Unique
 import org.spongepowered.asm.mixin.injection.At
 import org.spongepowered.asm.mixin.injection.Inject
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable
-import java.util.function.Predicate
+import java.util.function.*
 
 @Mixin(FallbackResourceManager::class)
 abstract class MixinFallbackResourceManager {
